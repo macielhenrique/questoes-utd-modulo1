afb5905d279fdfc81e2c84ce77165f73cdd51de4
macielhenrique <62320094+macielhenrique@users.noreply.github.com>
2021-03-15 23:21:19 -0300

    Add files via upload

---
+++ questoes.php
@@ -0,0 +1,71 @@
+<?php
+   #questão6
+
+           $nota1 = 9;
+              $nota2 = 7;
+                 $nota3 = 6;
+                    $nota4 = 5;
+                       $nota5 = 9;
+                          $nota6 = 6;
+                             $nota7 = 3;
+                                $nota8 = 5;
+                                     $nota9 = 2; 
+                                         $nota10 = 7;
+
+$media = ($nota1+ $nota2 + $nota3 + $nota+4 + $nota5 + $nota6 + $nota7 + $nota8 + $nota9 + $nota10) / 10;
+
+    echo  "questão 6"."<br>" ; 
+
+     echo "a media de notas é ?" .$media ."<br>"."<br>";
+
+
+#questão7
+
+      echo "questão 7". "<br>" ;
+
+        echo "o maior numero é :". max(15,25,35,45,100). "<br>"."<br>" ;
+
+
+
+
+
+
+
+       #questão8
+
+            echo "questão 8 ". "<br>" ."<br>" ;
+
+              $idade = array (15,17,18,20,25);
+
+                for ($w=0;$w<count($idade);$w++){
+                 if($idade[$w]>=18){
+                  echo "maior de 18  :".$idade[$w]."<br>". "<br>";
+                                            }  
+                                                   }    
+
+
+
+
+    #questão9
+      echo "questão 9 : "."<br>" ;
+     $nomes = array("zeca","maria","chiquim","aderbal","victor");
+       echo $nomes[4]."<br>";
+       echo $nomes[3]."<br>";
+       echo $nomes[2]."<br>";
+       echo $nomes[1]."<br>";
+       echo $nomes[0]."<br>"."<br>";
+
+    #questão10
+      echo "questão 10"."<br>";"<br>" ;
+        
+          $dia = 15;
+          $mes = 03;
+          $ano = 2021;
+
+          echo "hoje o dia é :".$dia. "<br>";
+          echo "do mês :  ".$mes."<br>";
+          echo "do ano de:  ".$ano."<br>";
+          
+
+
+?>
